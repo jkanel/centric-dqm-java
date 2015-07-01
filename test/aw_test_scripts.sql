138ac39767be56b2dcf67d01a4dd8317c555aaa1
jkanel <kanel.jeff@gmail.com>
2015-06-30 23:23:08 -0400

    Update

--- test/aw_test_scripts.sql
+++ test/aw_test_scripts.sql
@@ -1,12 +1,12 @@
 USE [dqm]
 GO
 
-
+-- drop table dqm.scenario
 
 
 /* ############################################### */
 
-DELETE FROM dqm.connection WHERE connection_uid IN ('AW2012','AWDW2012');
+DELETE FROM dqm.connection;
 GO
 
 INSERT INTO [dqm].[connection]
@@ -38,17 +38,41 @@ SELECT
 , 5 AS [timeout_sec]
 ,CURRENT_TIMESTAMP AS [create_dtm]
 
+UNION ALL
+
+SELECT
+'CAP_DW' AS [connection_uid]
+, 'com.microsoft.sqlserver.jdbc.SQLServerDriver' AS [jdbc_driver]
+, 'jdbc:sqlserver://172.16.2.7;databaseName=warehouse' AS [jdbc_url]
+, 'centric_developer' AS [username]
+, 'centric_developer' AS [password]
+, 5 AS [timeout_sec]
+,CURRENT_TIMESTAMP AS [create_dtm]
+
+
+UNION ALL
+
+SELECT
+'CAP_CEDW' AS [connection_uid]
+, 'com.microsoft.sqlserver.jdbc.SQLServerDriver' AS [jdbc_driver]
+, 'jdbc:sqlserver://172.16.2.7;databaseName=CentricEnterpriseDW' AS [jdbc_url]
+, 'centric_developer' AS [username]
+, 'centric_developer' AS [password]
+, 5 AS [timeout_sec]
+,CURRENT_TIMESTAMP AS [create_dtm]
+
+
 GO
 
 
 /* ############################################### */
 
-DELETE FROM dqm.scenario WHERE scenario_uid IN ('TEST1');
+DELETE FROM dqm.scenario;
 GO
 
 DECLARE 
-  @act VARCHAR(2000) = null
-, @exp VARCHAR(2000) = null;
+  @act VARCHAR(max) = null
+, @exp VARCHAR(max) = null;
 
 SET @exp = 'dbo.dqm_test @modulus=<<MODULUS>>, @modularity=<<MODULARITY>>;'
 
@@ -128,7 +152,7 @@ INSERT INTO [dqm].[scenario]
 SELECT
   'TEST1' AS [scenario_uid]
 , 'Test 1' AS [scenario_desc]
-, 'Axxxx,Bxxxx' AS [tag_list]
+, 'TEST' AS [tag_list]
 , 'year,month,state' AS [grain_list]
 , 3 AS [modulus]
 , 'AW2012' AS [expected_connection_uid]
@@ -141,13 +165,184 @@ SELECT
 , 'Y' AS [active_flag]
 , CURRENT_TIMESTAMP AS [create_dtm]
 
+GO
+
+DECLARE 
+  @act VARCHAR(max) = null
+, @exp VARCHAR(max) = null;
+
+SET @exp = '
+SELECT
+  x.year
+, x.month
+, x.operating_group
+
+, x.net_amt
+, x.income_amt
+, x.expense_amt
+, x.net_expense_amt
+, x.net_revenue_amt
+, x.cogs_amt
+  -- supress zero aggregates, replace with NULL
+, CASE WHEN x.net_cogs_amt = 0.0 THEN NULL ELSE x.net_cogs_amt END AS net_cogs_amt
+, x.reimb_expense_income_amt
+, x.reimb_expense_cost_amt
+, x.employee_cost_amt
+, x.contractor_cost_amt
+, x.w2t_cost_amt
+, x.gross_profit_amt
+, x.non_cogs_amt  
+, x.net_profit_amt
+, x.centric_cost_amt
+, x.centric_cost_transfer_amt
+  -- supress zero aggregates, replace with NULL
+, CASE WHEN x.net_centric_cost_amt = 0.0 THEN NULL ELSE x.net_centric_cost_amt END AS net_centric_cost_amt
+, x.bu_cost_amt
+  -- supress zero aggregates, replace with NULL
+, CASE WHEN x.operating_profit_amt = 0.0 THEN NULL ELSE x.operating_profit_amt END AS operating_profit_amt
+, x.shared_profit_transfer_amt
+
+FROM
+(
+
+  SELECT
+    year
+  , month
+  , BU AS operating_group
+
+  , SUM(NetAmt) AS net_amt
+  , SUM(CASE WHEN Category = ''Income'' THEN NetAmt END) AS income_amt
+  , -1*SUM(CASE WHEN Category = ''COGS'' and RollupAccount=''Expenses'' THEN NetAmt END) AS expense_amt
+  , -1*SUM(CASE WHEN RollupAccount=''Expenses'' THEN NetAmt END) AS net_expense_amt
+  , SUM(CASE WHEN RollupAccount = ''Revenue'' THEN NetAmt END) AS net_revenue_amt
+  , -1*SUM(CASE WHEN Category = ''COGS'' THEN NetAmt END) AS cogs_amt
+
+  , -1*(
+
+      ISNULL(SUM(CASE WHEN Category = ''COGS'' THEN NetAmt END), 0)
+      - ISNULL(SUM(CASE WHEN Category = ''Income'' and RollupAccount=''Expenses'' THEN NetAmt END), 0)
+
+     ) AS net_cogs_amt
+
+  , SUM(CASE WHEN Category = ''Income'' and RollupAccount=''Expenses'' THEN NetAmt END) AS reimb_expense_income_amt
+  , -1*SUM(CASE WHEN Category = ''Income'' and RollupAccount=''Expenses'' THEN NetAmt END) AS reimb_expense_cost_amt
+  , -1*SUM(CASE WHEN RollupAccount IN (''PartnerWages'',''Employees'') THEN NetAmt END) AS employee_cost_amt
+  , -1*SUM(CASE WHEN RollupAccount = ''Contractors and W2T'' AND Account=''Contractors-COGS'' THEN NetAmt END) AS contractor_cost_amt
+  , -1*SUM(CASE WHEN RollupAccount in (''Contractors and W2T'') AND Account != ''Contractors-COGS'' THEN NetAmt END) AS w2t_cost_amt
+  , SUM(CASE WHEN  Category IN (''Income'',''COGS'') THEN NetAmt END) AS gross_profit_amt
+  , -1*SUM(CASE WHEN Category = ''Non-COGS'' THEN NetAmt END)  AS non_cogs_amt 
+  , SUM(NetAmt) AS net_profit_amt
+  , -1* SUM(CASE WHEN Account=''National Cost Allocation'' AND BU NOT IN (''National'',''SSA'') THEN NetAmt END) AS centric_cost_amt
+  , -1* SUM(CASE WHEN Account=''National Cost Allocation'' AND BU IN (''National'',''SSA'') THEN NetAmt END) AS centric_cost_transfer_amt
+
+  , -1*(
+
+      ISNULL(SUM(CASE WHEN Account=''National Cost Allocation'' AND BU NOT IN (''National'',''SSA'') THEN NetAmt END), 0)
+      - ISNULL(SUM(CASE WHEN Account=''National Cost Allocation'' AND BU IN (''National'',''SSA'') THEN NetAmt END), 0)
+
+    ) AS net_centric_cost_amt
+
+  , -1*SUM(CASE WHEN Category = ''Non-COGS'' AND BU NOT IN (''National'',''SSA'') THEN NetAmt END) AS bu_cost_amt
+  , (
+  
+      ISNULL(SUM(CASE WHEN  Category IN (''Income'',''COGS'') THEN NetAmt END), 0)
+      - ISNULL(SUM(CASE WHEN Category = ''Non-COGS'' THEN NetAmt END), 0)
+
+    ) AS operating_profit_amt
+  , SUM(CASE WHEN RollupAccount = ''Shared Profit -Resource Share'' THEN NetAmt END) AS shared_profit_transfer_amt
+
+  FROM
+  CentricEnterpriseDW.dbo.QB_PL_Detail
+  WHERE
+  Year >= 2010
+  GROUP BY
+    year
+  , month
+  , BU
+
+) x
+'
+
+SET @act = '
+SELECT
+  c.year
+, c.month_of_year AS month
+, og.operating_group_desc AS operating_group
+
+, SUM(x.net_amt) AS net_amt
+, SUM(x.income_amt) AS income_amt
+, SUM(x.expense_amt) AS expense_amt
+, SUM(x.net_expense_amt) AS net_expense_amt
+, SUM(x.net_revenue_amt) AS net_revenue_amt
+, SUM(x.cogs_amt) AS cogs_amt
+, SUM(x.net_cogs_amt) AS  net_cogs_amt
+, SUM(x.reimb_expense_income_amt) AS reimb_expense_income_amt
+, SUM(x.reimb_expense_cost_amt) AS  reimb_expense_cost_amt
+, SUM(x.employee_cost_amt) AS employee_cost_amt
+, SUM(x.contractor_cost_amt) AS contractor_cost_amt
+, SUM(x.w2t_cost_amt) AS w2t_cost_amt
+, SUM(x.gross_profit_amt) AS gross_profit_amt
+, SUM(x.non_cogs_amt) AS   non_cogs_amt
+, SUM(x.net_profit_amt) AS net_profit_amt
+, SUM(x.centric_cost_amt) AS centric_cost_amt
+, SUM(x.centric_cost_transfer_amt) AS centric_cost_transfer_amt
+  -- supress zero aggregates, replace with NULL
+, SUM(x.net_centric_cost_amt) AS net_centric_cost_amt
+, SUM(x.bu_cost_amt) AS  bu_cost_amt
+  -- supress zero aggregates, replace with NULL
+, SUM(x.operating_profit_amt) AS  operating_profit_amt
+, SUM(x.shared_profit_transfer_amt) AS  shared_profit_transfer_amt
+
+FROM
+gl_tran x
+INNER JOIN dbo.operating_group og ON og.operating_group_key = x.operating_group_key
+INNER JOIN dbo.calendar c ON c.date_key = x.gl_tran_date_key
+WHERE
+c.year >= 2010
+GROUP BY
+  c.year
+, c.month_of_year
+, og.operating_group_desc
+'
+
+INSERT INTO [dqm].[scenario]
+           ([scenario_uid]
+           ,[scenario_desc]
+           ,[tag_list]
+           ,[grain_list]
+           ,[modulus]
+           ,[expected_connection_uid]
+           ,[expected_command]
+           ,[actual_connection_uid]
+           ,[actual_command]
+           ,[case_failure_record_limit]
+           ,[case_success_record_limit]
+           ,[allowed_case_failure_rate]
+           ,[active_flag]
+           ,[create_dtm])
+
+SELECT
+  'CAP_GL1' AS [scenario_uid]
+, 'CAP_GL1' AS [scenario_desc]
+, 'CAP' AS [tag_list]
+, 'year,month,operating_group' AS [grain_list]
+, 1 AS [modulus]
+, 'CAP_CEDW' AS [expected_connection_uid]
+, @exp AS [expected_command]
+, 'CAP_DW' AS [actual_connection_uid]
+, @act AS [actual_command]
+, 1000 AS [case_failure_record_limit]
+, 0 AS [case_success_record_limit]
+, 0.00 AS [allowed_case_failure_rate]
+, 'Y' AS [active_flag]
+, CURRENT_TIMESTAMP AS [create_dtm]
 
 GO
 
 
 /* ############################################### */
 
-DELETE FROM dqm.[scenario_measure] WHERE scenario_uid = ('TEST1') AND measure_name = 'sales_per_order';
+DELETE FROM dqm.[scenario_measure];
 GO
 
 
@@ -240,3 +435,5 @@ ORDER BY
 
 truncate table dqm.test
 truncate table dqm.test_case
+
+select * from dqm.scenario
